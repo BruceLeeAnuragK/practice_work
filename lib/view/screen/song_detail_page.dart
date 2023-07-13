245605ed15deba00bd1db26c966860d853244721
BruceLeeK <kanadeanurag87@gmail.com>
2023-07-13 12:58:41 +0530

    13/7/23 1

--- lib/view/screen/song_detail_page.dart
+++ lib/view/screen/song_detail_page.dart
@@ -9,7 +9,17 @@ class SongDetailPage extends StatefulWidget {
   State<SongDetailPage> createState() => _SongDetailPageState();
 }
 
-class _SongDetailPageState extends State<SongDetailPage> {
+class _SongDetailPageState extends State<SongDetailPage>
+    with TickerProviderStateMixin {
+  late AnimationController animationController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 400));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AudioProvider>(
@@ -25,95 +35,112 @@ class _SongDetailPageState extends State<SongDetailPage> {
                   Colors.purpleAccent,
                 ])),
               ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Slider(
-                      activeColor: Colors.purple,
-                      min: 0,
-                      max: provider.totalDuration.inSeconds.toDouble(),
-                      value: provider.currentPosition,
-                      onChanged: (val) {
-                        provider.seek(seconds: val.toInt());
-                      }),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                              "${snapShot.data!.inMinutes}:${(snapShot.data!.inSeconds) % 60}"),
-                        ),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                              "${provider.totalDuration.inMinutes} :${(provider.totalDuration.inSeconds) % 60}"),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Expanded(
-                        child: IconButton(
-                            onPressed: () {
-                              provider.skip(seconds: -10);
-                            },
-                            icon: const Icon(
-                              Icons.skip_previous,
-                              color: Colors.purple,
-                              size: 20,
-                            )),
-                      ),
-                      Expanded(
-                        child: StreamBuilder(
-                            stream: provider.assetsAudioPlayer.isPlaying,
-                            builder: (context, AsyncSnapshot<bool> snapShot) {
-                              snapShot.data!
-                                  ? animationController.forward()
-                                  : animationController.reverse();
-
-                              return IconButton(
-                                  onPressed: () {
-                                    if (snapShot.data!) {
-                                      provider.pause();
-                                      animationController.reverse();
-                                      setState(() {});
-                                    } else {
-                                      provider.play();
-                                      animationController.forward();
-                                      setState(() {});
-                                    }
-                                  },
-                                  icon: AnimatedIcon(
-                                    color: Colors.purple,
-                                    icon: AnimatedIcons.play_pause,
-                                    progress: animationController,
-                                  ));
-                            }),
-                      ),
-                      Expanded(
-                        child: IconButton(
-                          onPressed: () {
-                            provider.skip(seconds: 10);
-                          },
-                          icon: const Icon(
-                            Icons.skip_next,
-                            size: 20,
-                            color: Colors.purple,
+              StreamBuilder(
+                stream: provider.assetsAudioPlayer.currentPosition,
+                builder: (context, AsyncSnapshot<Duration> snapShot) {
+                  if (snapShot.hasData) {
+                    double currentPosition =
+                        snapShot.data!.inSeconds.toDouble();
+                    return SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Slider(
+                              activeColor: Colors.purple,
+                              min: 0,
+                              max: provider.totalDuration.inSeconds.toDouble(),
+                              value: currentPosition,
+                              onChanged: (val) {
+                                provider.seek(seconds: val.toInt());
+                              }),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                      "${snapShot.data!.inMinutes}:${(snapShot.data!.inSeconds) % 60}"),
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                      "${provider.totalDuration.inMinutes} :${(provider.totalDuration.inSeconds) % 60}"),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Expanded(
+                                child: IconButton(
+                                    onPressed: () {
+                                      provider.skip(seconds: -10);
+                                    },
+                                    icon: const Icon(
+                                      Icons.skip_previous,
+                                      color: Colors.purple,
+                                      size: 20,
+                                    )),
+                              ),
+                              Expanded(
+                                child: StreamBuilder(
+                                    stream:
+                                        provider.assetsAudioPlayer.isPlaying,
+                                    builder: (context,
+                                        AsyncSnapshot<bool> snapShot) {
+                                      snapShot.data!
+                                          ? animationController.forward()
+                                          : animationController.reverse();
+
+                                      return IconButton(
+                                          onPressed: () {
+                                            if (snapShot.data!) {
+                                              provider.pause();
+                                              animationController.reverse();
+                                              setState(() {});
+                                            } else {
+                                              provider.play();
+                                              animationController.forward();
+                                              setState(() {});
+                                            }
+                                          },
+                                          icon: AnimatedIcon(
+                                            color: Colors.purple,
+                                            icon: AnimatedIcons.play_pause,
+                                            progress: animationController,
+                                          ));
+                                    }),
+                              ),
+                              Expanded(
+                                child: IconButton(
+                                  onPressed: () {
+                                    provider.skip(seconds: 10);
+                                  },
+                                  icon: const Icon(
+                                    Icons.skip_next,
+                                    size: 20,
+                                    color: Colors.purple,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Divider(
+                            height: 3,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  Divider(
-                    height: 3,
-                  ),
-                ],
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
               ),
             ],
           ),
