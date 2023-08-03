ea2014f3206da1e3a24a915569eaf9ec4ddf834c
Mirror_Wall <kanadeanurag87@gmail.com>
2023-08-03 12:34:28 +0530

    03/08/2023 12:34

--- lib/view/screen/Drag&Drop_screen.dart
+++ lib/view/screen/Drag&Drop_screen.dart
@@ -12,74 +12,82 @@ class _DragScreenState extends State<DragScreen> {
   Widget build(BuildContext context) {
     bool dropped = false;
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Simple Drag and Drop",
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text(
+          "Simple Drag and Drop",
+          style: TextStyle(color: Colors.white),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Draggable(
-                data: "demo",
-                childWhenDragging: Container(
-                  height: 150,
-                  width: 150,
-                  color: Colors.grey,
-                ),
-                child: Container(
-                  height: 150,
-                  width: 150,
-                  color: Colors.red,
-                ),
-                feedback: Container(
-                  height: 150,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    border: Border.all(),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 10,
-                        color: Colors.grey,
-                        offset: Offset(5, 5),
-                      ),
-                    ],
-                  ),
-                ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Draggable(
+              data: "demo",
+              axis: Axis.vertical,
+              childWhenDragging: Container(
+                height: 150,
+                width: 150,
+                color: Colors.grey,
               ),
-              DragTarget(
-                builder: (context, _, __) => Container(
-                  height: 150,
-                  width: 150,
-                  alignment: Alignment.center,
-                  color: dropped ? Colors.red : Colors.grey,
-                  child: Text(dropped ? "Item Dropped" : "Drop here"),
-                ),
-                onWillAccept: (data) {
-                  return data == "demo";
-                },
-                onLeave: (val) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text("Missed !!"),
-                      behavior: SnackBarBehavior.floating,
-                      backgroundColor: Colors.red,
+              child: Container(
+                height: 150,
+                width: 150,
+                color: Colors.red,
+              ),
+              feedback: Container(
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  border: Border.all(),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 10,
+                      color: Colors.grey,
+                      offset: Offset(5, 5),
                     ),
-                  );
-                },
-                onAccept: (data) {
-                  setState(() {
-                    dropped = true;
-                  });
-                },
+                  ],
+                ),
               ),
-            ],
-          ),
-        ));
+            ),
+            DragTarget(
+              builder: (context, _, __) => Container(
+                height: 150,
+                width: 150,
+                alignment: Alignment.center,
+                color: dropped ? Colors.red : Colors.grey,
+                child: Text(dropped ? "Item Dropped" : "Drop here"),
+              ),
+              onWillAccept: (data) {
+                return data == "demo";
+              },
+              onLeave: (val) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Missed !!"),
+                    behavior: SnackBarBehavior.floating,
+                    backgroundColor: Colors.red,
+                  ),
+                );
+              },
+              onAccept: (data) {
+                setState(() {
+                  dropped = true;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () {
+          setState(() {});
+        },
+      ),
+    );
   }
 }
